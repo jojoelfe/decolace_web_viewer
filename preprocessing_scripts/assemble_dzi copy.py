6e62dabf0b0f64112a45e56c89700a9b85232bce
Johannes Elferich <jojotux123@hotmail.com>
2024-10-11 16:41:45 -0400

    copy

--- preprocessing_scripts/assemble_dzi copy.py
+++ preprocessing_scripts/assemble_dzi copy.py
@@ -5,12 +5,11 @@ import pyvips
 from pathlib import Path
 
 
-
-lamella = Path("/home/elferich/Pictures/montage.png")
-
-
-bl = pyvips.Image.new_from_file(lamella )
+for lamella in Path("/nrs/elferich/candida_project/Montages").glob("*/*_filtered.png"):
 
 
+    bl = pyvips.Image.new_from_file(lamella )
 
-bl.dzsave( Path("/home/elferich/Pictures/dzi") / "montage",suffix=".png")
+
+
+    bl.dzsave( Path("/nrs/elferich/candida_project/Montages/dzi/") / lamella.with_suffix("").name,suffix=".png")
